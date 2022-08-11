ace7417aede006475aee40e7ab8d4a875a54233b
DDD9 <dumitrescu549@gmail.com>
2022-08-11 18:17:09 +0300

    Last feature

--- github-gist-api/src/components/GistCard.js
+++ github-gist-api/src/components/GistCard.js
@@ -5,10 +5,13 @@ import { getAllForkers } from '../api/api_caller'
 const GistCard = (props) => {
 
     let tags = []
+    let gistContent = []
     const [forkers, setForkers] = useState(null)
+    const [showGistContent, setShowGistContent] = useState(false)
 
     useEffect(() => {  
-
+        // fetch the forkers data from the gist's api
+        // in which we'll find information about the users who forked the gist
         const fetchForkers = async () => {
             const forkersData = await getAllForkers(props.forks_url)
             let forkersArray = []
@@ -27,22 +30,34 @@ const GistCard = (props) => {
         fetchForkers() 
     }, [props.forks_url])
 
+    // save the file content and the tags in arrays to display them to the user
     for (let file in props.files) { 
         let tag = props.files[file].language
 
         if (! tags.includes(tag)) { 
             tags.push(tag)
         }
+
+        gistContent.push(file)
+    }
+
+    const gistClicked = () => {
+        // if the description of the gist has been clicked, it will either show the content on the screen or remove it from it (depending on the current state)
+        if (showGistContent === true) {
+            setShowGistContent(false)
+        } else {
+            setShowGistContent(true)
+        }
     }
 
     return (
         <div className="gist-card">
             <div className="description-wrapper">
-                <a href="localhost:3000" className="gist-description">{props.description.length > 0 ? props.description : "No description"}</a>
+                <div onClick={gistClicked} className="gist-description">{props.description.length > 0 ? props.description : "No description"}</div>
             </div>
 
-            <div className="tag-forks-wrapper">
-                <span className="gist-forkers-language">File types: </span>
+            <div className="tag-forks-content-wrapper">
+                <span className="gist-forkers-language-content">File types: </span>
                 {tags.map((tag, index) => {
                     return (
                         <span key={index} className="gist-tag">{tag}</span>
@@ -50,8 +65,8 @@ const GistCard = (props) => {
                 })}
             </div>
 
-            <div className="tag-forks-wrapper">
-                <span className="gist-forkers-language">Last forkers: </span>
+            <div className="tag-forks-content-wrapper">
+                <span className="gist-forkers-language-content">Last forkers: </span>
                 {forkers == null 
                 ?
                     <div>Loading...</div>
@@ -62,6 +77,18 @@ const GistCard = (props) => {
                         )
                     })}
             </div> 
+
+            {showGistContent 
+            && 
+            <div className="tag-forks-content-wrapper">
+                <span className="gist-forkers-language-content">Gist content(files): </span>
+                {gistContent.map((content, index) => {
+                    return (
+                        <span key={index} className="gist-content">{content}</span>
+                    )
+                })}
+            </div>
+            }
         </div>
     )
 }
